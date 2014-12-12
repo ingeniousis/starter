333d2130de2cb2716ac20c675e19346812554f9b
ingeniousis <ingeniousis@outlook.com>
2014-12-12 10:16:01 -0800

    Intermediate Updates

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -30,33 +30,35 @@ angular.module('starter.controllers', [])
 .controller('HomeCtrl', ['$scope',
     function ($scope) {}])
 
-.controller('AreaHomeCtrl', ['$scope', '$stateParams', 'ConfigService', 'UtilityService',
-    function ($scope, $stateParams, ConfigService, UtilityService) {
+.controller('AreaHomeCtrl', ['$scope', '$stateParams', '$state', '$timeout', 'ConfigService', 'UtilityService',
+    function ($scope, $stateParams, $state, $timeout, ConfigService, UtilityService) {
         $scope.id = $stateParams.areaId;
         $scope.UtilityService = UtilityService;
         ConfigService.GetAreaSettings($scope.id, function (data) {
             $scope.settings = data;
         });
 
-        $scope.showLoading = function () {
-            $ionicLoading.show({
-                template: 'Loading...'
-            });
-        };
+        $scope.onPackSelected = function (pack) {
+            $scope.$parent.showLoading();
+            var params = {
+                areaId: $scope.id,
+                packId: UtilityService.GetPackId(pack.name)
+            };
 
-        $scope.hideLoading = function () {
-            $ionicLoading.hide();
+            $timeout(function () {
+                $state.go('app.area.kp', params);
+            }, 10);
         };
 }])
 
 .controller('KnowledgePackCtrl', ['$scope', '$stateParams', '$timeout', 'ConfigService',
     function ($scope, $stateParams, $timeout, ConfigService) {
-        $scope.$parent.showLoading();
-
         $scope.areaId = $stateParams.areaId;
         ConfigService.GetAreaSettings($stateParams.areaId, function (data) {
             $scope.area = data;
             $scope.pack = ConfigService.GetPackSetting($scope.area, $stateParams.packId);
+
+            // Timeout to load the DOM
             $timeout(InitializeMap, 0);
         });
 
@@ -68,7 +70,10 @@ angular.module('starter.controllers', [])
                 $scope.$parent.hideLoading();
             };
 
-            simplemaps_usmap.load();
+            // Timeout for the nav-view animation to complete
+            $timeout(function () {
+                simplemaps_usmap.load();
+            }, 0);
         }
 }])
 
